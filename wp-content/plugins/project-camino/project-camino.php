3b75a0f7852510baaad35b217ee646c5becbffbf
Allen Heishman <allen@MiCasita-Studio.local>
2025-06-01 09:33:31 -0500

    Restore plugins folder and add Project Camino plugin

---
+++ wp-content/plugins/project-camino/project-camino.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Project Camino Management
+ * Description: Custom plugin for managing CDC weekends, registrations, roles, and pescadores.
+ * Version: 1.0
+ * Author: Allen Heishman
+ */
+
+defined('ABSPATH') or die('No script kiddies please!');
